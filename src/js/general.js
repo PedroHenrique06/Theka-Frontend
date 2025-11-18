325eebd119a9df28df48273341846592234289d3
PedroHenrique06 <pedrosales2806@gmail.com>
2025-11-17 23:13:54 -0300

    fix: correção do erro no menu profile.

--- src/js/general.js
+++ src/js/general.js
@@ -1,28 +1,25 @@
 const sideMenu = document.getElementById('side-menu');
 const sideMenuButton = document.getElementById('side-menu-button');
 const menuProfileButton = document.getElementById('profile-icon');
-const menuExitButton = document.getElementById('exit-icon');
+const menuExitButton = document.querySelector('.exit-icon-container');
 
 // Abre o menu lateral
 sideMenuButton.addEventListener('click', () => {
     sideMenu.style.display = sideMenu.style.display === 'flex' ? 'none' : 'flex';
 });
 
-// Verifica se o clique foi dentro do menu lateral
-document.addEventListener('click', (e) => {
-    if (!sideMenu.contains(e.target) && !sideMenuButton.contains(e.target)) {
-        sideMenu.style.display = 'none';
-    }
-});
-
-
-menuProfileButton.addEventListener('click', (e) => {
-    e.stopPropagation();
+// Abre o container do botão de sair
+menuProfileButton.addEventListener('click', (event) => {
+    event.stopPropagation();
     menuExitButton.style.display = menuExitButton.style.display === 'block' ? 'none' : 'block';
 });
 
-document.addEventListener('click', (e) => {
-    if (!menuExitButton.contains(e.target)) {
+// Verifica se o clique foi dentro do container escolhido
+document.addEventListener('click', (event) => {
+    if (!sideMenu.contains(event.target) && !sideMenuButton.contains(event.target)) {
+        sideMenu.style.display = 'none';
+    }
+    if (!menuExitButton.contains(event.target)) {
         menuExitButton.style.display  = 'none';
     }
 });
